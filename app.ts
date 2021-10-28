951a9ebdb086c54355a0ac08ad8aa2f1cb504d4c
Jose Alanya <jose.alanya@gmail.com>
2021-10-28 18:39:38 -0500

    FEAT: Typescript Basics and Basic Types: Working with Enums

--- app.ts
+++ app.ts
@@ -1,16 +1,24 @@
-const person: {
-  name: string;
-  age: number;
-  hobbies: string[];
-  role: [number, string];
-} = {
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string];
+// } = {
+//   name: 'Maximilian',
+//   age: 30,
+//   hobbies: ['Sports', 'Cooking'],
+//   role: [2, 'author']
+// };
+
+enum Role { ADMIN, READ_ONLY, AUTHOR}
+const person = {
   name: 'Maximilian',
   age: 30,
   hobbies: ['Sports', 'Cooking'],
-  role: [2, 'author']
+  role: Role.ADMIN
 };
 
-person.role.push('admin'); // This is gonna work! Unfortunately, .push does not catch the error
+// person.role.push('admin'); // This is gonna work! Unfortunately, .push does not catch the error
 // person.role[1] = 10; // it can't assign number value to string type definition
 // person.role = [0, 'admin', 'user'] // error due to: more elements
 
@@ -24,6 +32,10 @@ console.log(person.name);
 for (const hobby of person.hobbies) {
   console.log(hobby.toUpperCase())
   // console.log(hobby.map()) // !!! ERROR !!!
+}
+
+if (person.role === Role.AUTHOR) {
+  console.log('is author');
 }
 
 const product: {
