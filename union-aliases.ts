d7b2dbe082ca020ec6b4061a5d6a5bc9080becf2
Jose Alanya <jose.alanya@gmail.com>
2021-11-01 18:15:30 -0500

    FEAT: Typescript Basics and Basic Types: Function Return Types & 'void'

---
+++ union-aliases.ts
@@ -0,0 +1,51 @@
+type Combinable = number | string;
+type ConversionDescriptor = 'as-number' | 'as-text';
+
+function combine(
+  input1: Combinable,
+  input2: Combinable,
+  resultConversion: ConversionDescriptor,
+) {
+  let result;
+  if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
+    result = +input1 + +input2;
+  } else {
+    result = input1.toString() + input2.toString();
+  }
+  return result;
+//   if (resultConversion === 'as-number') {
+//     return +result;
+//   } else {
+//     return result.toString();
+//   }
+}
+
+const combinedAges = combine(30, 26, 'as-number');
+console.log(combinedAges);
+
+const combinedStringAges = combine('30', '26', 'as-number');
+console.log(combinedStringAges);
+
+const combinedNames = combine('Max', 'Anna', 'as-text');
+console.log(combinedNames);
+
+type User = { name: string; age: number };
+const u1: User = { name: 'Max', age: 30 }; // This works!
+
+// You can simplify this code.
+function greet(user: { name: string, age: number}) {
+  console.log('Hi, I am ' + user.name);
+}
+
+function isOlder(user: { name: string, age: number}, checkAge: number) {
+  return checkAge > user.age;
+}
+
+// To
+function newGreet(user: User) {
+  console.log('Hi, I am ' + user.name);
+}
+
+function newIsOlder(user: User, checkAge: number) {
+  return checkAge > user.age;
+}
