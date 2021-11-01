19bf2f41facd2524deaa15b94c2e647e5eb61c21
Jose Alanya <jose.alanya@gmail.com>
2021-11-01 18:33:30 -0500

    FEAT: Typescript Basics and Basic Types: Functions as Types

--- app.ts
+++ app.ts
@@ -7,3 +7,12 @@ function printResult(num: number): void {
 }
 
 printResult(add(5, 12));
+
+//let combinedValues: Function;
+let combinedValues: (a: number, b: number) => number;
+
+combinedValues = add;
+// combinedValues = 5; // error if you assign a number value to a function type;
+// combinedValues = printResult; // no compile error and the result is undefined when combinedValues is defined as general Function definition.
+// combinedValues = printResult; // compile error when combinedValues is defined using a specify Function definition.
+console.log(combinedValues(8, 8));
