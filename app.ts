449ed744c111df2ae673e25eed2fcc13119c0f23
Jose Alanya <jose.alanya@gmail.com>
2021-11-01 18:43:46 -0500

    FEAT: Typescript Basics and Basic Types: Function Types & Callbacks

--- app.ts
+++ app.ts
@@ -6,6 +6,11 @@ function printResult(num: number): void {
   console.log('Result: ' + num);
 }
 
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+  const result = n1 + n2;
+  cb(result);
+}
+
 printResult(add(5, 12));
 
 //let combinedValues: Function;
@@ -16,3 +21,7 @@ combinedValues = add;
 // combinedValues = printResult; // no compile error and the result is undefined when combinedValues is defined as general Function definition.
 // combinedValues = printResult; // compile error when combinedValues is defined using a specify Function definition.
 console.log(combinedValues(8, 8));
+
+addAndHandle(10, 20, (result) => {
+  console.log(result);
+});
