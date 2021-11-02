47f0b1e49b2d92004c3ccde2f4c2f7ae55506a82
Jose Alanya <jose.alanya@gmail.com>
2021-11-01 19:04:32 -0500

    FEAT: Typescript Basics and Basic Types: The 'never' type

--- app.ts
+++ app.ts
@@ -8,3 +8,8 @@ if (typeof userInput === 'string') {
   userName = userInput;
 }
 
+function generateError(message: string, code: number): never {
+  throw { message: message, errorCode: code };
+}
+
+generateError('An error occurred!', 500);
